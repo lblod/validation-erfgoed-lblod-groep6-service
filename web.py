749e835772b227e18f427c9b790ad7b5142d057f
Peter Mika <Peter.Mika@ordina.nl>
2024-09-12 14:44:36 +0200

    Access SPARQL endpoint

--- web.py
+++ web.py
@@ -1,15 +1,17 @@
 import os
+from flask import Flask, request  
+import traceback
 from importlib import import_module
 import builtins
-
-import flask
 from rdflib.namespace import Namespace
-
-import helpers
 from escape_helpers import sparql_escape
 
+from string import Template
+from helpers import query, logger
+from escape_helpers import sparql_escape_uri
+
 # WSGI variable name used by the server
-app = flask.Flask(__name__)
+service = Flask(__name__)
 
 ##################
 ## Vocabularies ##
@@ -24,6 +26,7 @@ builtins.app = app
 builtins.helpers = helpers
 builtins.sparql_escape = sparql_escape
 
+
 # Import the app from the service consuming the template
 app_file = os.environ.get('APP_ENTRYPOINT')
 try:
@@ -32,9 +35,42 @@ try:
 except Exception:
     helpers.logger.exception('Exception raised when importing app code')
 
+
+@app.route('/')
+def test():
+    return('We zijn in de lucht')
+
+@app.route('/validate', methods=['GET'])
+def index():
+    try:	
+        my_person = "http://example.com/me"
+        query_template = Template("""
+        PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+        SELECT ?name
+        WHERE {
+            $person a foaf:Person ;
+                foaf:firstName ?name .
+        }
+        """)
+        query_string = query_template.substitute(person=sparql_escape_uri(my_person))
+        query_result = query(query_string)
+        return query_result
+        #return(request.values['id'])
+    
+    except:
+        service.logger.error(traceback.format_exc())
+        return {'error': 'General exception'}    
+        
+        
+
+
+
+
 #######################
 ## Start Application ##
 #######################
 if __name__ == '__main__':
     debug = os.environ.get('MODE') == "development"
-    app.run(debug=debug, host='0.0.0.0', port=80)
+    service.run(debug=debug, host='0.0.0.0', port=80)
